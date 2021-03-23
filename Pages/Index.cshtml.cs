5df630cd595664ab8da48c631d9be0f8619e7b69
Jeremiah Chienda <jeremiahchienda@gmail.com>
2021-03-23 20:54:23 +0200

    reading cart from PageModel

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -17,9 +17,36 @@ namespace shopping_cart.Pages
             _logger = logger;
         }
 
+        public List<Item> Products { get; set; }
+
+
         public void OnGet()
         {
+            var items = new List<Item>() {
+                new Item() {
+                    Name = "Bearded Oil",
+                    Description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur dolor ratione culpa! Deleniti vel",
+                    ImageUrl = "https://images.unsplash.com/photo-1564020426549-fabfb8c467ad?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80",
+                    Price = 7000
+                },
+                new Item() {
+                    Name = "Whole Wheat Bread",
+                    Description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur dolor ratione culpa! Deleniti vel",
+                    ImageUrl = "https://images.unsplash.com/photo-1593668387570-52c91ffb4d74?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80",
+                    Price = 3000
+                }
+            };
 
+            Products = items;
         }
+    }
+
+    public class Item
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public Decimal Price { get; set; }
     }
 }
